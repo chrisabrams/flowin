1a8851761141ef4785316761fddc02d1fa16ab79
Chris Abrams <mail@chrisabrams.com>
2012-06-21 00:20:07 -0700

    Disabled stylus-sprite as it sucks.

--- src/flow.js
+++ src/flow.js
@@ -6,7 +6,7 @@ var fs  = require('fs'),
 	exec = require('child_process').exec,
 	//sass = require(__dirname + '/../node_modules/node-sass/sass'),
 	stylus = require('stylus'),
-	StylusSprite = require("stylus-sprite"),
+	//StylusSprite = require("stylus-sprite"),
 	util = require('util'),
 	wrench = require('wrench');
 
